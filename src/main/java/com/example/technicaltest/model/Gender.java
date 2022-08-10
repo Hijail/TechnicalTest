2dd755bae9dbf508a0f5ece9c10b7cd948b99c67
Hijail <baptiste.gellato@epitech.eu>
2022-08-10 17:01:57 +0200

    :feat: add new model User, Country and Gender

---
+++ src/main/java/com/example/technicaltest/model/Gender.java
@@ -0,0 +1,23 @@
+package com.example.technicaltest.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "gender")
+public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    private String gender;
+
+    public Gender() {
+    }
+
+    public Gender(String gender) {
+        this.gender = gender;
+    }
+}
