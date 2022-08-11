260d128445f20b45e14455e7e176942d23541892
Hijail <baptiste.gellato@epitech.eu>
2022-08-11 18:27:35 +0200

    :test: add gender check test

--- src/test/java/com/example/technicaltest/service/concretions/UserServiceTest.java
+++ src/test/java/com/example/technicaltest/service/concretions/UserServiceTest.java
@@ -2,7 +2,9 @@ package com.example.technicaltest.service.concretions;
 
 import com.example.technicaltest.exception.InvalidBirthdateException;
 import com.example.technicaltest.exception.InvalidCountryException;
+import com.example.technicaltest.exception.InvalidGenderException;
 import com.example.technicaltest.model.Country;
+import com.example.technicaltest.model.Gender;
 import com.example.technicaltest.model.User;
 import com.example.technicaltest.repository.CountryRepository;
 import com.example.technicaltest.repository.UserRepository;
@@ -34,11 +36,20 @@ public class UserServiceTest {
 
     private Country country;
 
+    private Gender gender;
+
     private Country InitCountry(String countryName) {
         country = new Country();
         country.setCountry(countryName);
 
         return country;
+    }
+
+    private Gender InitGender(String genderType) {
+        gender = new Gender();
+        gender.setGender(genderType);
+
+        return gender;
     }
 
     /**
@@ -124,5 +135,92 @@ public class UserServiceTest {
             userService.createUser(user);
         });
         assertEquals("Null parameters are not allowed", exception.getMessage());
+    }
+
+    @Test
+    public void testFirstValidGender() {
+        final User user = new User("validUser");
+        User create = null;
+        Gender male = InitGender("male");
+
+        user.setBirthdate(new GregorianCalendar(2000, Calendar.FEBRUARY, 21).getTime());
+        user.setCountry(InitCountry("France"));
+        user.setGender(male);
+        given(countryRepository.findByCountry("France")).willReturn(InitCountry("France"));
+        try {
+            create = userService.createUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(user.getGender().getGender(), create.getGender().getGender());
+    }
+
+    @Test
+    public void testSecondValidGender() {
+        final User user = new User("validUser");
+        User create = null;
+        Gender female = InitGender("female");
+
+        user.setBirthdate(new GregorianCalendar(2000, Calendar.FEBRUARY, 21).getTime());
+        user.setCountry(InitCountry("France"));
+        user.setGender(female);
+        given(countryRepository.findByCountry("France")).willReturn(InitCountry("France"));
+        try {
+            create = userService.createUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(user.getGender().getGender(), create.getGender().getGender());
+    }
+
+    @Test
+    public void testThirdValidGender() {
+        final User user = new User("validUser");
+        User create = null;
+        Gender other = InitGender("other");
+
+        user.setBirthdate(new GregorianCalendar(2000, Calendar.FEBRUARY, 21).getTime());
+        user.setCountry(InitCountry("France"));
+        user.setGender(other);
+        given(countryRepository.findByCountry("France")).willReturn(InitCountry("France"));
+        try {
+            create = userService.createUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(user.getGender().getGender(), create.getGender().getGender());
+    }
+
+    @Test
+    public void testNullGender() {
+        final User user = new User("validUser");
+        User create = null;
+
+        user.setBirthdate(new GregorianCalendar(2000, Calendar.FEBRUARY, 21).getTime());
+        user.setCountry(InitCountry("France"));
+        user.setGender(null);
+        given(countryRepository.findByCountry("France")).willReturn(InitCountry("France"));
+        try {
+            create = userService.createUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(user.getGender(), create.getGender());
+    }
+
+    @Test
+    public void testInvalidGender() {
+        final User user = new User("validUser");
+        User create = null;
+        Gender invalid = InitGender("invalid");
+
+        user.setBirthdate(new GregorianCalendar(2000, Calendar.FEBRUARY, 21).getTime());
+        user.setCountry(InitCountry("France"));
+        user.setGender(invalid);
+        given(countryRepository.findByCountry("France")).willReturn(InitCountry("France"));
+        InvalidGenderException exception = assertThrows(InvalidGenderException.class, () -> {
+            userService.createUser(user);
+        });
+        assertEquals("Only male / female / other or empty are allow for gender", exception.getMessage());
     }
 }
