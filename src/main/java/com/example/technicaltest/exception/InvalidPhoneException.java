ce1ca2b4d6b1a26ef027a29e373c285615659dd8
Hijail <baptiste.gellato@epitech.eu>
2022-08-15 17:06:17 +0200

    :feat: add AOP log

--- src/main/java/com/example/technicaltest/exception/InvalidPhoneException.java
+++ src/main/java/com/example/technicaltest/exception/InvalidPhoneException.java
@@ -1,6 +1,12 @@
 package com.example.technicaltest.exception;
 
 public class InvalidPhoneException extends RuntimeException {
+
+    /**
+     * Exception for user services method when phone is invalid
+     *
+     * @param errorMessage
+     */
     public InvalidPhoneException(String errorMessage) {
         super(errorMessage);
     }
